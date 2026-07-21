2274e60376c60a6058ac38f5a54d58ccc7380e72
Nafi <soul24346@gmail.com>
2026-07-22 02:21:22 +0530

    Use Railway MySQL variables

--- server/config/database.js
+++ server/config/database.js
@@ -2,15 +2,15 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
- {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
     logging: false,
-}
+  }
 );
 
 module.exports = sequelize;
